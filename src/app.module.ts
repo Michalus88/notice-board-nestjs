6738f90b7e001b2838738a72b240abaac9640508
Michal Molenda <michalus88@gmail.com>
2022-05-17 21:44:57 +0200

    generated ad module, services and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdsModule } from './ads/ads.module';
+import { AdModule } from './ad/ad.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), AdsModule, AdModule],
   controllers: [AppController],
   providers: [AppService],
 })
